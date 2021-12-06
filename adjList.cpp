1c0a02938c6eb60f2249a555442c01e0eba41bea
Alan Cardiel <acard6@illinois.edu>
2021-12-06 15:22:02 -0600

    graph data structure 70% done

--- adjList.cpp
+++ adjList.cpp
@@ -3,20 +3,75 @@
 #include <string>
 #include <vector>
 #include <cmath>
+#include <utility>
+#include <iostream>
 
 #include "adjList.h"
 
 using namespace std;
 
+adjList::adjList(){
+	size = 7699
+	list = new pair<Airport,edge*>*[size]
+	for (size_t i=0; i<size; i++){
+		list[i] = NULL;
+	}
+}
+
 void ADJList::addEdge(Airport airport, Route route){
     
 }
 
+void ADJList::addVertex(vector<Airport> airpotList){
+	for (int i=0; i < airpotList.size(); i++){
+		Airport place = airportList[i];
+		int idx = hashFunc(place);
+		int id2 = doubleHash(airportList[i])
+		while list[idx] != NULL){
+			idx = (idx + id2) % size;
+		}
+		list[idx] = new pair<place, NULL>;
+	}
+}
+
+int ADJList::hashFunc(Airport airport){
+	string name = airport.name;
+	unsigned int h = 0;
+	//int index = (int)name[0]*676+(int)name[1]*26+(int)name[2]-45695;
+	//index = index % list.size();
+	for (size_t i=0; i<name.length(); ++i)
+		h = 33 * h + name[i];
+	return h % list->size();
+}
+
+int ADJList::doubleHash(Airport airport){
+	string name = airport.name;
+	unsigned int h = 5381;
+	for (size_t i=0; i<name.length(); ++i)
+		h = 31* h + name[i];
+
+	return (h % (list->size() - 1)) + 1;
+}
+
+int ADJList::find(Airport airport){
+	int idx = hashFunc(airport);
+	int id2 = doublehash(airport);
+	int original = idx;
+	while (list[idx]->first.name != airport.name){
+		idx = (idx + id2) % size;
+		if (idx == original){
+			return -1;
+		}
+	}
+	
+	return idx;
+}
+
 double ADJList::distance(Airport air1, Airport air2){
 	
-	int EarthRadiusKm = 6371;
+	radius = 6371;
 
-	double toRad = M_PI / 180.0;
+	toRad = M_PI / 180.0;
 	double latRad1 = air1.latitude * toRad;
 	double latRad2 = air2.latitude * toRad;
 	double lonRad1 = air1.longitude * toRad;
@@ -26,5 +81,5 @@ double ADJList::distance(Airport air1, Airport air2){
 	double doffLo = lonRad2 - lonRad1;
 
 	double computation = asin(sqrt(sin(diffLa / 2) * sin(diffLa / 2) + cos(latRad1) * cos(latRad2) * sin(doffLo / 2) * sin(doffLo / 2)));
-	return 2 * EarthRadiusKm * computation;
+	return 2 * radius * computation;
 }
--- adjList.h
+++ adjList.h
@@ -16,24 +16,57 @@ class ADJList{
      */
     
     private:
-        struct adjVert{
-            string vertex;
-            adjVert* next; 
-        };
+        /** 
+        *   using a hash table to store the airport ID'd in a searchable way
+        *   with the keys are the vertices as an airport loation and values as
+        *   a linked list of the edges to adjecent vertecies containing
+        *   
+        *   from there
+        */
         struct edge {
-            string start, end;
-            int weight;
+            Airport end; // airport connected to the key
+            edge* next; //edge pointer pointing to another connected edge
+            int weight; //weight from key to this edge
         };
 
-        vector<string> list;
-        double radius; //earths in miles
+        //pair of doubly liked airport and linked edges 
+        std::pair<Airport,edge*>** list;
+        size_t size;
+        double radius; //earths in kilometers
         double toRad; // multiplier conversion from degrees to radians 
 
     public:
         /** adds the edges that each airport is connected to 
         *   and the edge weight 
         */
+        
+        /** 
+         * default constructor for the graph
+         * uses a doubly lined list as a hashtable.
+         * used code from dhhashtable from lab_hash
+         */
+        adjList();
+
+        //add an edge to an existing vertext
         void addEdge(Airport airport, Route route);
+
+        //adds a vertext to the hash table
+        void addVertex(vector<Airport> airportList);
+
+        /**
+         * hash function for so that airports are easy to look for and
+         * so that fins doesnt take O(n^2)
+         */
+        int hashFunc(Airport airport);
+
+        //double hash function to help deal with collisions
+        int doubleHash(Airport airport)
+
+        /**
+         * uses hash function to help locate where in the table an
+         * airport is
+         */
+        int find(Airport airport);
 
         /**
          * calcultes how far each connected airport is from eachother
